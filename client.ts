303b677f5a4b366b7d986097500cc2bc231d8420
Thomas Papamichail <thomas@pointer.gr>
2026-04-28 12:56:30 +0300

    Refactor Client to use namespaced API structure with admin and client endpoints

--- client.ts
+++ client.ts
@@ -1,12 +1,96 @@
 import { Configuration } from "./openapi";
-import { AddonsApi, CompaniesApi, CouponsApi, DomainCategoriesApi, DomainContactsApi, IntegrationsApi, InvoiceContactsApi, InvoicesApi, IssuesApi, ItemsApi, OrdersApi, PoliciesApi, ProductCategoriesApi, ProductsApi, TemplatesApi, TldsApi, UsersApi } from "./openapi/api";
+import {
+  AddonsApi,
+  ClientInvoiceContactsApi,
+  ClientItemsApi,
+  ClientOrdersApi,
+  ClientUsersApi,
+  CompaniesApi,
+  CouponsApi,
+  DomainCategoriesApi,
+  DomainContactsApi,
+  GroupRolesApi,
+  IntegrationsApi,
+  InvoiceContactsApi,
+  InvoicesApi,
+  IssuesApi,
+  ItemsApi,
+  OrdersApi,
+  PricePolicyApi,
+  ProductCategoriesApi,
+  ProductsApi,
+  TemplatesApi,
+  TldsApi,
+  TransactionsApi,
+  UsersApi,
+} from "./openapi/api";
 
 // This is the entrypoint for the openapi package
 export * from './openapi/index';
 
+class AdminNamespace {
+  private _addons?: AddonsApi;
+  private _companies?: CompaniesApi;
+  private _coupons?: CouponsApi;
+  private _domainCategories?: DomainCategoriesApi;
+  private _domainContacts?: DomainContactsApi;
+  private _groupRoles?: GroupRolesApi;
+  private _integrations?: IntegrationsApi;
+  private _invoiceContacts?: InvoiceContactsApi;
+  private _invoices?: InvoicesApi;
+  private _issues?: IssuesApi;
+  private _items?: ItemsApi;
+  private _orders?: OrdersApi;
+  private _pricePolicy?: PricePolicyApi;
+  private _productCategories?: ProductCategoriesApi;
+  private _products?: ProductsApi;
+  private _templates?: TemplatesApi;
+  private _tlds?: TldsApi;
+  private _transactions?: TransactionsApi;
+  private _users?: UsersApi;
+
+  constructor(private readonly configuration: Configuration) {}
+
+  get addons() { return this._addons ??= new AddonsApi(this.configuration); }
+  get companies() { return this._companies ??= new CompaniesApi(this.configuration); }
+  get coupons() { return this._coupons ??= new CouponsApi(this.configuration); }
+  get domainCategories() { return this._domainCategories ??= new DomainCategoriesApi(this.configuration); }
+  get domainContacts() { return this._domainContacts ??= new DomainContactsApi(this.configuration); }
+  get groupRoles() { return this._groupRoles ??= new GroupRolesApi(this.configuration); }
+  get integrations() { return this._integrations ??= new IntegrationsApi(this.configuration); }
+  get invoiceContacts() { return this._invoiceContacts ??= new InvoiceContactsApi(this.configuration); }
+  get invoices() { return this._invoices ??= new InvoicesApi(this.configuration); }
+  get issues() { return this._issues ??= new IssuesApi(this.configuration); }
+  get items() { return this._items ??= new ItemsApi(this.configuration); }
+  get orders() { return this._orders ??= new OrdersApi(this.configuration); }
+  get pricePolicy() { return this._pricePolicy ??= new PricePolicyApi(this.configuration); }
+  get productCategories() { return this._productCategories ??= new ProductCategoriesApi(this.configuration); }
+  get products() { return this._products ??= new ProductsApi(this.configuration); }
+  get templates() { return this._templates ??= new TemplatesApi(this.configuration); }
+  get tlds() { return this._tlds ??= new TldsApi(this.configuration); }
+  get transactions() { return this._transactions ??= new TransactionsApi(this.configuration); }
+  get users() { return this._users ??= new UsersApi(this.configuration); }
+}
+
+class ClientNamespace {
+  private _invoiceContacts?: ClientInvoiceContactsApi;
+  private _items?: ClientItemsApi;
+  private _orders?: ClientOrdersApi;
+  private _users?: ClientUsersApi;
+
+  constructor(private readonly configuration: Configuration) {}
+
+  get invoiceContacts() { return this._invoiceContacts ??= new ClientInvoiceContactsApi(this.configuration); }
+  get items() { return this._items ??= new ClientItemsApi(this.configuration); }
+  get orders() { return this._orders ??= new ClientOrdersApi(this.configuration); }
+  get users() { return this._users ??= new ClientUsersApi(this.configuration); }
+}
+
 export class Client {
   private basePath: string;
-  private configuration?: Configuration;
+  private configuration: Configuration;
+  private _admin?: AdminNamespace;
+  private _client?: ClientNamespace;
 
   constructor(url?: string) {
     if (url) {
@@ -24,91 +108,14 @@ export class Client {
   }
 
   public setAccessToken(accessToken: string) {
-    this.configuration!.accessToken = accessToken;
+    this.configuration.accessToken = accessToken;
   }
 
-  public addons() {
-    const addonsApi = new AddonsApi(this.configuration);
-    return addonsApi;
+  public get admin(): AdminNamespace {
+    return this._admin ??= new AdminNamespace(this.configuration);
   }
 
-  public companies() {
-    const companiesApi = new CompaniesApi(this.configuration);
-    return companiesApi;
-  }
-
-  public coupons() {
-    const couponsApi = new CouponsApi(this.configuration);
-    return couponsApi;
-  }
-
-  public domainCategories() {
-    const domainCategoriesApi = new DomainCategoriesApi(this.configuration);
-    return domainCategoriesApi;
-  }
-
-  public domainContacts() {
-    const domainContactsApi = new DomainContactsApi(this.configuration);
-    return domainContactsApi;
-  }
-
-  public integrations() {
-    const integrationsApi = new IntegrationsApi(this.configuration);
-    return integrationsApi;
-  }
-
-  public invoiceContacts() {
-    const invoiceContactsApi = new InvoiceContactsApi(this.configuration);
-    return invoiceContactsApi;
-  }
-
-  public invoices() {
-    const invoicesApi = new InvoicesApi(this.configuration);
-    return invoicesApi;
-  }
-
-  public issues() {
-    const issuesApi = new IssuesApi(this.configuration);
-    return issuesApi;
-  }
-
-  public items() {
-    const itemsApi = new ItemsApi(this.configuration);
-    return itemsApi;
-  }
-
-  public orders() {
-    const ordersApi = new OrdersApi(this.configuration);
-    return ordersApi;
-  }
-
-  public policies() {
-    const policiesApi = new PoliciesApi(this.configuration);
-    return policiesApi;
-  }
-
-  public productCategories() {
-    const productCategoriesApi = new ProductCategoriesApi(this.configuration);
-    return productCategoriesApi;
-  }
-
-  public products() {
-    const productsApi = new ProductsApi(this.configuration);
-    return productsApi;
-  }
-
-  public templates() {
-    const templatesApi = new TemplatesApi(this.configuration);
-    return templatesApi;
-  }
-
-  public tlds() {
-    const tldsApi = new TldsApi(this.configuration);
-    return tldsApi;
-  }
-
-  public users() {
-    const usersApi = new UsersApi(this.configuration);
-    return usersApi;
+  public get client(): ClientNamespace {
+    return this._client ??= new ClientNamespace(this.configuration);
   }
 }
